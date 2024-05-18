1814b69ea4f55c73632f41ba561c1ab83fd5cb8b
khattaflow <thememestay@gmail.com>
2024-05-18 06:48:21 +0530

    Update knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,35 +1,52 @@
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
-// Function to solve the 0-1 knapsack problem
-int knapsack(int W, vector<int>& wt, vector<int>& val, int n) {
-    vector<vector<int>> K(n + 1, vector<int>(W + 1));
+// Function to find the maximum of two integers
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
 
-    // Build table K[][] in bottom-up manner
+// Function to solve the 0/1 Knapsack problem
+int knapsack(int W, int wt[], int val[], int n) {
+    // Create a 2D array to store the maximum value that can be obtained
+    // for the first i items with a knapsack capacity of j
+    int dp[n + 1][W + 1];
+
+    // Initialize the dp array with 0
     for (int i = 0; i <= n; i++) {
-        for (int w = 0; w <= W; w++) {
-            if (i == 0 || w == 0)
-                K[i][w] = 0;
-            else if (wt[i - 1] <= w)
-                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
-            else
-                K[i][w] = K[i - 1][w];
+        for (int j = 0; j <= W; j++) {
+            dp[i][j] = 0;
         }
     }
 
-    // The maximum value that can be obtained
-    return K[n][W];
+    // Build dp array in a bottom-up manner
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= W; j++) {
+            // If the current item's weight is more than the current capacity j,
+            // then the current item cannot be included in the knapsack
+            if (wt[i - 1] > j) {
+                dp[i][j] = dp[i - 1][j];
+            }
+            // Otherwise, choose the maximum of including the current item or excluding it
+            else {
+                dp[i][j] = max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+            }
+        }
+    }
+
+    // The result is stored at dp[n][W]
+    return dp[n][W];
 }
 
+// Main function
 int main() {
-    int W = 50; // Capacity of knapsack
-    vector<int> val = {60, 100, 120}; // Values of items
-    vector<int> wt = {10, 20, 30}; // Weights of items
-    int n = val.size(); // Number of items
+    int val[] = {60, 100, 120};
+    int wt[] = {10, 20, 30};
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
 
     cout << "Maximum value that can be obtained: " << knapsack(W, wt, val, n) << endl;
 
     return 0;
 }
-
